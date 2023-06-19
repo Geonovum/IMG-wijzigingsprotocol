2139029e6250c4ff6c120cfe21d24b16438ca242
MichelGrothe <112104525+MichelGrothe@users.noreply.github.com>
2023-06-19 11:00:28 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,57 @@
-var respecConfig =
+let respecConfig = 
 {
-  subtitle: "Informatiemodel Geluid",
-  pubDomain: "img",
-  specStatus: "GN-WV",
+  useLogo: true,
+  useLabel: true,
+  // title is verplicht! Neem hier de titel van het document op ----------------------
+  title: "Wijzigingsprotocol informatiemodel Geluid",
+  //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
+  specStatus: "DEF",                   // Werkversie
+  //specStatus: "cv",               // Consultatieversie
+  //specStatus: "vv",               // Versie ter vaststelling
+  //specStatus: "def",              // Vastgestelde versie
+  //specStatus: "basis",            // Basis Document
+  //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
+  //specType: "NO",                 // Norm
+  //specType: "ST",                 // Standaard
+  //specType: "IM",                 // Informatie Model
+  //specType: "PR",                 // Praktijkrichtlijn
+  specType: "HR",                     // HandReiking
+  //specType: "WA",                 // Werkafspraak
+  //specType: "BD",                 // Beheer Documentatie
+  //specType: "AL",                 // Algemeen document
+  //specType: "BP",                 // Best Practice
   specType: "BD",
-  license: "cc-by-nd",
-  //publishDate: "2023-02-23",
-  authors: 
-  [
+  //-- pubDomain is verplicht! (komt in de URL) -------------------------------------
+  pubDomain: "img",
+  //-- license: voor de geldende gebruiksvoorwaarden. Default is cc-by.
+  //licence: "cc-by-nd",            // bronvermelding, geen afgeleide werken (default)
+  //licence: "cc0",                 // Public Domain Dedication
+  licence: "cc-by-nd",              // Attribution, met bronvermelding
+  //-- shortName is verplicht! (komt in de URL: kies logische afkorting)--------------
+  shortName: "IMG-wijzigingsprotocol",
+  //-- publishDate is verplicht -------------------------------------------------------
+  //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
+  //publishDate: "2023-03-28",
+  //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
+  //publishVersion: "0.0.2",
+  //previousVersion: "0.0.1",
+  //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
+  //previousPublishDate: "2014-05-01",
+  //previousMaturity: "CV",
+  //-- de namen van de Editor(s) / Redacteur(en)---------------------------------------
+  //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
+  editors:
+    [
+    {
+      name: "Michel Grothe",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+    ],
+  //-- de namen van de auteur(s) ------------------------------------------------------
+  //-- vul in: per auteur: name:, company:, companyURL: -------------------------------
+  authors:
+    [
     {
       name: "Pieter Bresters",
       company: "Geonovum",
@@ -38,22 +82,14 @@ var respecConfig =
       company: "Geonovum",
       companyURL: "https://www.geonovum.nl/"
     },   
-  ],
-  editors: 
-  [
-    {
-      name: "Michel Grothe",
-      company: "Geonovum",
-      companyURL: "https://www.geonovum.nl/"
-    }
-  ],
-  shortName: "IMG-wijzigingsprotocol",
-  format: "markdown",
+    ],
+  //neem hier de URL van de github repository op waar het respec document in staat
   github: "https://github.com/Geonovum/IMG-wijzigingsprotocol/",
-  issueBase: "https://github.com/Geonovum/IMG-wijzigingsprotocol/issues/",
-  edDraftURI: "https://Geonovum.github.io/IMG-wijzigingsprotocol/",
-  //generatedSubtitle: "Werkversie 10 augustus 2022", 
-  //previousPublishDate: "",  	    
-  //previousMaturity: "GN-CV", 
-
-}
+  // Create PDF and link to file in header (optional):
+  alternateFormats: [
+      {
+          label: "pdf",
+          uri: "template.pdf",
+      },
+  ],
+};
