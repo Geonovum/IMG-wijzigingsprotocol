733e40f086c1904bd0f3f83688c4a142b7395980
Wilko Quak <w.quak@geonovum.nl>
2023-09-19 16:25:13 +0200

    Dit is de versie die ik ga publiceren

--- js/config.js
+++ js/config.js
@@ -30,13 +30,13 @@ let respecConfig =
   shortName: "IMG-wijzigingsprotocol",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2023-03-28",
+  //publishDate: "2023-06-08",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  //previousPublishDate: "2014-05-01",
-  //previousMaturity: "CV",
+  previousPublishDate: "2023-06-08",
+  previousMaturity: "def",
   //-- de namen van de Editor(s) / Redacteur(en)---------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors:
@@ -85,10 +85,4 @@ let respecConfig =
   //neem hier de URL van de github repository op waar het respec document in staat
   github: "https://github.com/Geonovum/IMG-wijzigingsprotocol/",
   // Create PDF and link to file in header (optional):
-  alternateFormats: [
-      {
-          label: "pdf",
-          uri: "template.pdf",
-      },
-  ],
 };
