7838370b40259c95e1947579371f405b49ed5d99
Pieter Bresters <80040145+PB-GNM@users.noreply.github.com>
2023-11-20 09:33:44 +0100

    Update config.js
    Pub datum terug naar 8 juni 2023

--- js/config.js
+++ js/config.js
@@ -5,10 +5,10 @@ let respecConfig =
   // title is verplicht! Neem hier de titel van het document op ----------------------
   title: "Wijzigingsprotocol informatiemodel Geluid",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  specStatus: "WV",                 // Werkversie
+  //specStatus: "WV",                 // Werkversie
   //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
-  //specStatus: "def",              // Vastgestelde versie
+  specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
   //-- specType is verplicht bij alle andere dan BASIS ---------------------------------
   //specType: "NO",                 // Norm
@@ -30,13 +30,13 @@ let respecConfig =
   shortName: "IMG-wijzigingsprotocol",
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  //publishDate: "2023-11-14",
+  publishDate: "2023-06-08",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   //publishVersion: "0.0.2",
   //previousVersion: "0.0.1",
   //-- Voor dit blok geldt: alleen als er eerdere versies zijn en altijd beiden aan/uit! 
-  previousPublishDate: "2023-11-14",
-  previousMaturity: "def",
+  //previousPublishDate: "2023-11-14",
+  //previousMaturity: "def",
   //-- de namen van de Editor(s) / Redacteur(en)---------------------------------------
   //-- vul in: per Editor: name:, company:, companyURL: -------------------------------
   editors:
